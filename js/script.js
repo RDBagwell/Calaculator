198adbf857761b03d1399d077a05efdd183e69d7
Robert Bagwell <erbagwell@yahoo.com>
2020-09-17 22:39:24 -0600

    Operator Functionality

--- js/script.js
+++ js/script.js
@@ -2,12 +2,26 @@ const calculatorDisplay = document.querySelector('h1');
 const inputBtns = document.querySelectorAll('button');
 const clearBtn = document.getElementById('clear-btn');
 
+let firstValue = 0;
+let operatorValue = '';
+let awaitingValue = false;
+
 function sendNumberValue(number) {
-    const displayValue = calculatorDisplay.textContent;
-    calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    if(awaitingValue){
+        calculatorDisplay.textContent = number;
+        awaitingValue = false;
+    } else {
+        const displayValue = calculatorDisplay.textContent;
+        calculatorDisplay.textContent = displayValue === '0' ? number : displayValue + number;
+    }
+
 }
 
 function addDecimal() {
+    if(awaitingValue){
+        return;
+    }
+
     if(!calculatorDisplay.textContent.includes('.')){
         calculatorDisplay.textContent = `${calculatorDisplay.textContent}.`;
     };
@@ -15,13 +29,28 @@ function addDecimal() {
 
 function clearDisplay() {
     calculatorDisplay.textContent = '0';
+    firstValue = 0;
+    operatorValue = '';
+    awaitingValue = false;
+}
+
+function useOperator(operator) {
+    const currentValue = Number(calculatorDisplay.textContent);
+    if(!firstValue){
+        firstValue = currentValue;
+    } else{
+        console.log('cv: ', currentValue);
+    }
+    awaitingValue = true;
+    operatorValue = operator;
+    console.log('fv: ', firstValue, ' ov: ', operatorValue)
 }
 
 inputBtns.forEach((inputBtn)=>{
     if(inputBtn.classList.length === 0){
         inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
     } else if(inputBtn.classList.contains('operator')){
-        inputBtn.addEventListener('click', () => sendNumberValue(inputBtn.value));
+        inputBtn.addEventListener('click', () => useOperator(inputBtn.value));
     } else if(inputBtn.classList.contains('decimal')){
         inputBtn.addEventListener('click', () => addDecimal());
     }
